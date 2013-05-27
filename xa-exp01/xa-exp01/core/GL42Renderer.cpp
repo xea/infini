c45e660135de56dc2cdc38b2a02f73185f48fefe
xea <alex.pecsi@gmail.com>
2013-05-27 20:52:11 +0200

    Lua 5.2.2 project source added

--- xa-exp01/xa-exp01/core/GL42Renderer.cpp
+++ xa-exp01/xa-exp01/core/GL42Renderer.cpp
@@ -21,24 +21,21 @@ void GL42Renderer::prepareScene() {
 	Cube *cube2 = new Cube();
 	Cube *cube3 = new Cube();
 	Cube *cube4 = new Cube();
-	Cube *cube5 = new Cube();
-	Cube *cube6 = new Cube();
 
+	cube->translate(glm::vec3(1.3, 0, 0));
 	cube2->translate(glm::vec3(-1.3, 0, 0));
 	cube3->translate(glm::vec3(0, -1.3, 0));
 	cube4->translate(glm::vec3(-1.3, -1.3, 0));
-	cube5->translate(glm::vec3(1.3, -1.3, 0));
-	cube6->translate(glm::vec3(1.3, 0, 0));
+	square->translate(glm::vec3(1.3, -1.3, 0));
+	
 
 	scene = new Scene();
-	//scene->objects.push_back(triangle);
-	//scene->objects.push_back(square);
+	scene->objects.push_back(triangle);
+	scene->objects.push_back(square);
 	scene->objects.push_back(cube);
 	scene->objects.push_back(cube2);
 	scene->objects.push_back(cube3);
 	scene->objects.push_back(cube4);
-	scene->objects.push_back(cube5);
-	scene->objects.push_back(cube6);
 
 	projectionMatrix = glm::perspective(60.0f, (float) 1024 / (float) 768, 0.1f, 100.f);
 	viewMatrix = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
@@ -59,7 +56,7 @@ void GL42Renderer::drawScene() {
 	
 	glfwSwapBuffers();
 
-	//std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
 
 void GL42Renderer::destroyScene() {
@@ -79,7 +76,7 @@ void GL42Renderer::bindObject(RenderObject *object) {
 	glBufferData(GL_ARRAY_BUFFER, object->colorsCount() * 3 * sizeof(GLfloat), object->colors(), GL_STATIC_DRAW);
 	glVertexAttribPointer((GLuint) 1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(1);
-	glBindVertexArray(1);
+	//glBindVertexArray(1);
 	
 }
 
@@ -90,7 +87,7 @@ void GL42Renderer::drawObject(RenderObject *object) {
 
 	ffmod += 0.05f;
 
-	object->rotate(glm::vec3(ffmod, ffmod, 0));
+	//object->rotate(glm::vec3(ffmod, ffmod, 0));
 	//object->translate(glm::vec3(0.01f, 0, 0));
 	//object->scale(glm::vec3(0.01f, 0, 0));
 
